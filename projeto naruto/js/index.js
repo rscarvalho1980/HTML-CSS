83041c573a13dd189c56fb761e183f21d40db53d
Rodrigo Carvalho <eng.rscarvalho1980@gmail.com>
2023-10-13 22:38:12 +0100

    js finalizado

--- projeto naruto/js/index.js
+++ projeto naruto/js/index.js
@@ -1,4 +1,22 @@
-//passo1//
 const botoes = document.querySelectorAll('.botao');
+const personagens = document.querySelectorAll(".personagem");
 
-//passo2//
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
+        
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+})
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
